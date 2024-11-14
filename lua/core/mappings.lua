ace9b2a2669c71c5262c7585681feb7c283a23e2
Topjer <c.schmaeche@gmx.de>
2024-11-14 23:04:17 +0100

    Update mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -18,7 +18,7 @@ vim.keymap.set('n', '<leader>wq', '<cmd>q<CR>', { desc = '[W]indow [q]uit' })
 vim.keymap.set('n', '<leader>wc', '<cmd>bd<CR>', { desc = '[W]indow [c]lose buffer' })
 
 --vim.keymap.set('n', '<leader>w', '<C-w>', { desc = "Open [w]indow options"})
-vim.keymap.set('n', '<leader>to', '<cmd>bo term<CR>', { desc = '[T]erminal [o]pen' })
+vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm<CR>', { desc = '[T]oggle [T]erminal' })
 vim.keymap.set('n', '<leader>tp', '<cmd>bo term python<CR>', { desc = '[T]erminal [P]ython' })
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 --vim.keymap.set('t', '<C-w>', "<C-\\><C-n><C-w>")
