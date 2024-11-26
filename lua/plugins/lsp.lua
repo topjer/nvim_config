d31390a28c13013db5b57fef71d8b589c14e6c02
Topjer <c.schmaeche@gmx.de>
2024-11-26 13:00:42 +0100

    Reconfigure autocompletion for python

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,9 +10,9 @@ return {
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
     -- local capabilities = vim.lsp.protocol.make_client_capabilities()
     -- capabilities = vim.tbl_deep_extend('force', capabilities, require("cmp_nvim_lsp").default_capabilities())
-    -- nvim_lsp.pyright.setup {
-    --   capabilities = capabilities
-    -- }
+    nvim_lsp.pyright.setup {
+      capabilities = capabilities
+    }
     nvim_lsp.lua_ls.setup {
       capabilities = capabilities,
       settings = {
