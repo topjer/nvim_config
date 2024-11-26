e13b9533cb2a3898b8d0a2ea1e642111a0b66855
Topjer <c.schmaeche@gmx.de>
2024-11-26 13:00:13 +0100

    Improve auto completion for lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,9 +22,17 @@ return {
           },
           diagnostics = {
             globals = {'vim'}
+          },
+          workspace = {
+            library = {
+              [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+              [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+            }
           }
+
         }
       }
     }
   end,
+
 }
