82138c037405bc8660dbe4518adc74ef979d89a5
Topjer <c.schmaeche@gmx.de>
2024-12-01 21:54:07 +0100

    Update toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,8 +5,8 @@ return {
     config = function ()
       require("toggleterm").setup()
       local Terminal = require("toggleterm.terminal").Terminal
-      local python = Terminal:new({cmd = "python", direction="float", id = 2, display_name = "python"})
-      local standard = Terminal:new({ direction="float", id = 1, display_name = "standard"})
+      local python = Terminal:new({cmd = "python", direction="float", id = 2, display_name = "python", autochdir = true})
+      local standard = Terminal:new({ direction="float", id = 1, display_name = "standard", autochdir = true})
 
       local function _python_toggle()
         python:toggle()
@@ -20,14 +20,16 @@ return {
         local path = vim.fn.expand("%")
         local current_dir = vim.fs.dirname(path) .. '/'
         _standard_toggle()
-        vim.cmd('TermExec direction=float cmd="cd '.. current_dir .. ' && python solution.py"')
+        vim.cmd('TermExec direction=float cmd="clear && python solution.py"')
+        -- vim.cmd('TermExec direction=float cmd="cd '.. current_dir .. ' && clear && python solution.py"')
       end
 
       local function _advent_of_code_run_pytest()
         local path = vim.fn.expand("%")
         local current_dir = vim.fs.dirname(path) .. '/'
         _standard_toggle()
-        vim.cmd('TermExec direction=float cmd="cd '.. current_dir .. ' && pytest -s solution.py"')
+        -- vim.cmd('TermExec direction=float cmd="cd '.. current_dir .. ' && pytest -s solution.py"')
+        vim.cmd('TermExec direction=float cmd="clear && pytest -s solution.py"')
       end
 
       vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm direction=float<CR>', { desc = '[T]oggle [T]erminal' })
